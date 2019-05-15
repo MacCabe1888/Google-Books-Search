aa158e6048daa4c11585de5d83b9cbe58f1a72a6
Cameron <cameronjmaccabe@gmail.com>
2019-05-14 21:12:09 -0700

    edited comments

--- server.js
+++ server.js
@@ -4,20 +4,21 @@ const routes = require("./routes");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 
-// Configure body parsing for AJAX requests
+// Configure body parsing for AJAX requests:
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets
+// Serve up static assets:
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
+// Add routes, both API and view:
 app.use(routes);
 
-// Connect to MongoDB
+// Connect to MongoDB:
 const localDB = "google_books_db";
 mongoose.connect(
+  // Use localDB if not connected to deployment DB:
   process.env.MONGODB_URI || `mongodb://localhost/${localDB}`,
   {
     useCreateIndex: true,
@@ -25,7 +26,7 @@ mongoose.connect(
   }
 );
 
-// Start the API server
+// Start the API server:
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
