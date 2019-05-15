88d2d9b1d87d5a56238e76a55f13f6b960d1f0d8
Cameron <cameronjmaccabe@gmail.com>
2019-05-14 21:09:41 -0700

    set up default nav bar display and conditional page rendering using React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+// Here is the layout of the app.
+function App() {
+  return (
+    // using React Router to handle navigation through the site
+    <Router>
+      <div>
+        {/* The Nav bar is present on every page. */}
+        <Nav />
+        {/* The Switch tag is used for conditional page rendering based on the specified path. */}
+        <Switch>
+          {/* The "/" path renders the Home page where the user can search for books. */}
+          <Route exact path="/" component={Home} />
+          {/* The "/saved" path renders the Saved page where the user can view all books saved in the database. */}
+          <Route exact path="/saved" component={Saved} />
+          {/* Any path that matches none of the above renders the NoMatch page, which displays a 404 error message. */}
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
