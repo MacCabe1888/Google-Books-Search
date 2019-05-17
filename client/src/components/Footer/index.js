18657a1c6a91ed47010839da427b9e5a9870af73
Cameron <cameronjmaccabe@gmail.com>
2019-05-16 23:56:00 -0700

    styled footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,8 +4,8 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <hr />
-      <p className="pull-right">
+      <hr style={{ borderTop: "2px solid #79b6f2", background: "transparent" }} />
+      <p className="pull-right" style={{ color: "#79b6f2" }}>
         <i className="fab fa-react fa-lg" /> Powered by React.js
       </p>
     </footer>
